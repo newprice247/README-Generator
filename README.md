028b009c3fa81403d3d286ea6503e02d20668298
newprice247 <newprice247@gmail.com>
2023-08-26 11:25:22 -0400

    added functionality

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# README-Generator
-Module 9 challenge
+{
+
+"title": "j",
+
+"description": "j",
+
+"installation": "f",
+
+"usage": "e",
+
+"contribution": "a",
+
+"credits": "c",
+
+"license": [
+
+
+"BSD",
+
+
+"General Public License(GPL)",
+
+
+"GNU Lesser General Public License (LGPL)"
+
+],
+
+"test": "e",
+
+"username": "h",
+
+"email": "v"
+}
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
-const generateMarkdown = require('./utils/generateMarkdown.js')
+// const generateMarkdown = require('./utils/generateMarkdown.js')
 const fs = require('fs');
 // TODO: Create an array of questions for user input
 
@@ -37,7 +37,7 @@ const questions = [
         name: 'credits'
     },
     {
-        type: 'list',
+        type: 'checkbox',
         name: 'license',
         message: 'Which license(s) are you using for this project?',
         choices: ['MIT', 'Apache', 'BSD', 'General Public License(GPL)', 'GNU Lesser General Public License (LGPL)', 'Mozilla Public License (MPL)']
@@ -62,9 +62,13 @@ const questions = [
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
-
+inquirer.prompt(questions)
+        .then((data) => {
+            fs.writeFile('readme.md', JSON.stringify(data, null, '\n') , function (err) {
+            err ? console.log(err) : console.log('Success!')
+        })})
 // // TODO: Create a function to initialize app
 // function init() {}
 
 // // Function call to initialize app
-// init();
+// init()
